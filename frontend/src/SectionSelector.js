e29b69cf66ed9ddc7509744098fe3fe226c06a26
tquy <quynguyenthe000@gmail.com>
2021-09-25 22:30:10 +0700

    add number-subsection for section Selector component

--- frontend/src/SectionSelector.js
+++ frontend/src/SectionSelector.js
@@ -4,18 +4,24 @@ import { capitalize } from "./Base";
 function SectionSelector() {
     const [sectionStates, setSectionStates] = useState({
         "basic-info": false,
+        "number-basic-info": 1,
         "education": false,
+        "number-education": 1,
         "employment": false,
+        "number-employment": 1,
         "project": false,
+        "number-project": 1,
         "certificates": false,
+        "number-certificates": 1,
         "skills": false,
+        "number-skills": 1,
     })
 
     const handleChange = (e) => {
         console.log(e.target.name);
         let  target = e.target;
         let newSectionStates = Object.assign({}, sectionStates);
-        newSectionStates[target.name] = target.type === "checkbox" ? target.checked : target.value;
+        newSectionStates[target.name] = target.type === "checkbox" ? target.checked : Math.max(1,target.value);
         setSectionStates(newSectionStates);
     }
 
@@ -26,19 +32,19 @@ function SectionSelector() {
             </button> 
             <ul className="dropdown-menu" aria-labelledby="sectionSelector">
                 <li>
-                    <Section name={"basic-info"} checked={sectionStates["basic-info"]} onChange={handleChange} />
-                    <Section name={"education"} checked={sectionStates["education"]} onChange={handleChange} />
-                    <Section name={"employment"} checked={sectionStates["education"]} onChange={handleChange} />
-                    <Section name={"project"} checked={sectionStates["education"]} onChange={handleChange} />
-                    <Section name={"certificates"} checked={sectionStates["education"]} onChange={handleChange} />
-                    <Section name={"skills"} checked={sectionStates["education"]} onChange={handleChange} />
+                    <Section name={"basic-info"} checked={sectionStates["basic-info"]} number_subsection={sectionStates["number-basic-info"]} onChange={handleChange} />
+                    <Section name={"education"} checked={sectionStates["education"]} number_subsection={sectionStates["number-education"]} onChange={handleChange} />
+                    <Section name={"employment"} checked={sectionStates["employment"]} number_subsection={sectionStates["number-employment"]} onChange={handleChange} />
+                    <Section name={"project"} checked={sectionStates["project"]} number_subsection={sectionStates["number-project"]} onChange={handleChange} />
+                    <Section name={"certificates"} checked={sectionStates["certificates"]} number_subsection={sectionStates["number-certificates"]} onChange={handleChange} />
+                    <Section name={"skills"} checked={sectionStates["skills"]} number_subsection={sectionStates["number-skills"]} onChange={handleChange} />
                 </li>
             </ul>
         </div>
     )
 }
 
-function Section({ name, checked, onChange }) {
+function Section({ name, checked, number_subsection, onChange }) {
     let box_name = `${name}-checkbox`;
     let number_name = `number-${name}`;
     let label_name = name.split("-").map(word => capitalize(word)).join(" ");
@@ -54,8 +60,8 @@ function Section({ name, checked, onChange }) {
                 type="number" 
                 className="numberSubSection"
                 name={number_name}
-                value={1}
-                readOnly={true}
+                value={number_subsection}
+                onChange={(e) => onChange(e)}
             />
         </div>
     )
