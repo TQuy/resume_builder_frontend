74f273d7b231270aaa327e06d15f0402bd2e166a
tquy <quynguyenthe000@gmail.com>
2021-09-30 00:57:57 +0700

    avoid re-render all child component with useReducer and React.memo

--- frontend/src/SectionSelector.js
+++ frontend/src/SectionSelector.js
@@ -1,29 +1,30 @@
-import { useState, useCallback } from "react";
+import React, { useReducer } from "react";
 import { capitalize } from "./Base";
 
-function SectionSelector() {
-    const [sectionStates, setSectionStates] = useState({
-        "basic-info": false,
-        "number-basic-info": 1,
-        "education": false,
-        "number-education": 1,
-        "employment": false,
-        "number-employment": 1,
-        "project": false,
-        "number-project": 1,
-        "certificates": false,
-        "number-certificates": 1,
-        "skills": false,
-        "number-skills": 1,
-    })
+const initialState = {
+    "basic-info": false,
+    "number-basic-info": 1,
+    "education": false,
+    "number-education": 1,
+    "employment": false,
+    "number-employment": 1,
+    "project": false,
+    "number-project": 1,
+    "certificates": false,
+    "number-certificates": 1,
+    "skills": false,
+    "number-skills": 1,
+};
 
-    const handleChange = (e) => {
-        console.log(e.target.name);
-        let  target = e.target;
-        let newSectionStates = Object.assign({}, sectionStates);
-        newSectionStates[target.name] = target.type === "checkbox" ? target.checked : Math.max(1,target.value);
-        setSectionStates(newSectionStates);
-    }
+function reducer(state, action) {
+    const newState = {...state, [action.name] :action.value};
+    console.log(`action`, action);
+    console.log(`newState: ${newState}`);
+    return newState
+}
+
+function SectionSelector() {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
         <div id="navigation" className="dropdown">
@@ -32,28 +33,33 @@ function SectionSelector() {
             </button> 
             <ul className="dropdown-menu" aria-labelledby="sectionSelector">
                 <li>
-                    <Section name={"basic-info"} checked={sectionStates["basic-info"]} number_subsection={sectionStates["number-basic-info"]} onChange={handleChange} />
-                    <Section name={"education"} checked={sectionStates["education"]} number_subsection={sectionStates["number-education"]} onChange={handleChange} />
-                    <Section name={"employment"} checked={sectionStates["employment"]} number_subsection={sectionStates["number-employment"]} onChange={handleChange} />
-                    <Section name={"project"} checked={sectionStates["project"]} number_subsection={sectionStates["number-project"]} onChange={handleChange} />
-                    <Section name={"certificates"} checked={sectionStates["certificates"]} number_subsection={sectionStates["number-certificates"]} onChange={handleChange} />
-                    <Section name={"skills"} checked={sectionStates["skills"]} number_subsection={sectionStates["number-skills"]} onChange={handleChange} />
+                    <MemorizedSection name={"basic-info"} checked={state["basic-info"]} number_subsection={state["number-basic-info"]} onChange={dispatch} />
+                    <MemorizedSection name={"education"} checked={state["education"]} number_subsection={state["number-education"]} onChange={dispatch} />
+                    <MemorizedSection name={"employment"} checked={state["employment"]} number_subsection={state["number-employment"]} onChange={dispatch} />
+                    <MemorizedSection name={"project"} checked={state["project"]} number_subsection={state["number-project"]} onChange={dispatch} />
+                    <MemorizedSection name={"certificates"} checked={state["certificates"]} number_subsection={state["number-certificates"]} onChange={dispatch} />
+                    <MemorizedSection name={"skills"} checked={state["skills"]} number_subsection={state["number-skills"]} onChange={dispatch} />
                 </li>
             </ul>
         </div>
     )
 }
 
-function Section({ name, checked, number_subsection, onChange }) {
+const MemorizedSection = React.memo(function Section({ name, checked, number_subsection, onChange }) {
     let box_name = `${name}-checkbox`;
     let number_name = `number-${name}`;
     let label_name = name.split("-").map(word => capitalize(word)).join(" ");
+
+    const handleChange = (e) => {
+        const value = e.target.type === 'checkbox'? e.target.checked : Math.max(1, e.target.value);
+        return onChange({ name: e.target.name, value: value });
+    }
     return(
         <div className="form-check">
                 <input className="form-check-input" type="checkbox" id={box_name}
                 name={name}
                 checked={checked}
-                onChange={(e) => onChange(e)}
+                onChange={(e) => handleChange(e)}
                 />
             <label className="form-check-label" htmlFor={box_name}>{label_name}</label>
             <input 
@@ -61,10 +67,10 @@ function Section({ name, checked, number_subsection, onChange }) {
                 className="numberSubSection"
                 name={number_name}
                 value={number_subsection}
-                onChange={(e) => onChange(e)}
+                onChange={(e) => handleChange(e)}
             />
         </div>
     )
-}
+})
 
 export default SectionSelector
