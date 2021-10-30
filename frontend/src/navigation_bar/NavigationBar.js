2b0b8f65ace0251cdd7b95f81ee86830eea9eb06
tquy <quynguyenthe000@gmail.com>
2021-10-30 20:23:19 +0700

    hide nav bar when print

--- frontend/src/navigation_bar/NavigationBar.js
+++ frontend/src/navigation_bar/NavigationBar.js
@@ -16,22 +16,22 @@ export default function NavigationBar({ authToken, setAuthToken }) {
     const Logout = <a className="nav-link" onClick={handleLogout}>Log out</a>;
     const in_or_out = authToken ? Logout : Login
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav me-auto">
-                    <NavLink className="nav-link" to="/resume/" activeClassName="active" aria-current="page">
-                        Home
-                    </NavLink>
-                </div>
-                <div className="navbar-nav ms-auto">
-                    {in_or_out}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-print-none">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav me-auto">
+                        <NavLink className="nav-link" to="/resume/" activeClassName="active" aria-current="page">
+                            Home
+                        </NavLink>
+                    </div>
+                    <div className="navbar-nav ms-auto">
+                        {in_or_out}
+                    </div>
                 </div>
             </div>
-        </div>
         </nav>
     )
 }
