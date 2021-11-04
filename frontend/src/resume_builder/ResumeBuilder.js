1a8bf15a32c1465e76896faa0ba070369a2240ec
tquy <quynguyenthe000@gmail.com>
2021-11-05 02:59:46 +0700

    preserve state

--- frontend/src/resume_builder/ResumeBuilder.js
+++ frontend/src/resume_builder/ResumeBuilder.js
@@ -15,25 +15,34 @@ import DeleteButton from "./delete_button/DeleteButton";
 import PrintButton from "./print_button/PrintButton";
 import Alert from "./alert/Alert";
 
-const initial_control_state = {
-    "basic-info": {"checked": false, "number_subsection": 1, "payload": []},
-    "education": {"checked": false, "number_subsection": 1, "payload": []},
-    "employment": {"checked": false, "number_subsection": 1, "payload": []},
-    "projects": {"checked": false, "number_subsection": 1, "payload": []},
-    "certificates": {"checked": false, "number_subsection": 1, "payload": []},
-    "skills": {"checked": false, "number_subsection": 1, "payload": []},
+const initial_control_state = (preservedState) => {
+    if (preservedState) {
+        return JSON.parse(preservedState)
+    } else {
+        return {
+            "basic-info": {"checked": false, "number_subsection": 1, "payload": []},
+            "education": {"checked": false, "number_subsection": 1, "payload": []},
+            "employment": {"checked": false, "number_subsection": 1, "payload": []},
+            "projects": {"checked": false, "number_subsection": 1, "payload": []},
+            "certificates": {"checked": false, "number_subsection": 1, "payload": []},
+            "skills": {"checked": false, "number_subsection": 1, "payload": []},
+        }
+    }
 };
 
 function reducer(state, action) {
     switch(action.name) {
         case "blank":
+            sessionStorage.setItem('control_state', JSON.stringify(initial_control_state));
             return initial_control_state;
         case 'load':
+            sessionStorage.setItem('control_state', JSON.stringify(action.value));
             return action.value;
         default:
             // avoid shallow copy
             const newSectionState = {...state[action.name], [action.key]: action.value};
             const newState = {...state, [action.name]: newSectionState};
+            sessionStorage.setItem('control_state', JSON.stringify(newState));
             return newState
     }
 };
@@ -43,7 +52,7 @@ export const DispatchContext = createContext();
 function ResumeBuilder({ authToken }) {
     const [resumeList, setResumeList] = useState([]);
     const [currentResume, setCurrentResume] = useState({'name': '', 'id': 0});
-    const [control_state, dispatch] = useReducer(reducer, initial_control_state);
+    const [control_state, dispatch] = useReducer(reducer, sessionStorage.getItem('control_state'), initial_control_state);
     const [alertContent, setAlertContent] = useState('');
 
     useEffect(() => {
